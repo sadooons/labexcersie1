e991f8b6228f3fb9672aae838f571e24957cf969
khadijahalsabah <khadijahalsabah@localhost>
2026-09-23 20:45:58 +0300

    commit update

--- src/scemex/mystring.java
+++ src/scemex/mystring.java
@@ -1,5 +1,22 @@
 package scemex;
-
 public class mystring {
+private String Text; public mystring() {
+Text = null;}
 
+public void setString(String t){ Text = t;
+}
+
+public String invert( ){
+int i , n = Text.length();
+char [] Temp;
+Temp = Text.toCharArray(); Text= "";
+for ( i=0; i<n; i++) {
+if (Temp[i] >= 'A' && Temp[i] <= 'Z')
+Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
+else if (Temp[i] >= 'a' && Temp[i] <= 'z')
+Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
+
+Text += Temp[i]; }
+return Text;
+}
 }
